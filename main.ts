a113543793f12f90a489fa3cd318cf86338a02e2
TPAMarvin <31587596+TPAMarvin@users.noreply.github.com>
2020-07-02 13:34:57 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,11 +5,12 @@ input.onButtonPressed(Button.A, function () {
     strip.show()
 })
 input.onButtonPressed(Button.B, function () {
-    strip.showRainbow(1, 350)
+    strip2.showRainbow(1, 350)
     basic.pause(2000)
     strip.clear()
     strip.show()
 })
+let strip2: neopixel.Strip = null
 let strip: neopixel.Strip = null
 strip = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
-let strip2 = neopixel.create(DigitalPin.P1, 5, NeoPixelMode.RGB)
+strip2 = neopixel.create(DigitalPin.P1, 5, NeoPixelMode.RGB)
